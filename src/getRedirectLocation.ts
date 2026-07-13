54c8aadaf439bf7af0a98122ba5640960167fa59
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2026-07-13 13:22:54 +0900

    remove the cache configuration from `getRedirectLocation`
    It is no longer necessary, given the switch to the method of writing out to R2.

--- src/getRedirectLocation.ts
+++ src/getRedirectLocation.ts
@@ -1,18 +1,12 @@
 import { env } from "cloudflare:workers";
 import type { Result } from "./Result.js";
 
-const CACHE_TTL = 3 * 60; // 3 minutes
-
 export const getRedirectLocation = async (url: string): Promise<Result<string>> => {
   const response = await fetch(url, {
     method: "HEAD",
     redirect: "manual",
     headers: {
       "User-Agent": env.USER_AGENT,
-    },
-    cf: {
-      cacheEverything: true,
-      cacheTtl: CACHE_TTL,
     },
   });
 
